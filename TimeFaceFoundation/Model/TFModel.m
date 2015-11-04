3bdd90cc1d11adbd13e9a6ddc452820fe138a715
zguanyu <zguanyu@zguanyus-MacBook-Pro.local>
2015-11-04 10:31:20 +0800

    remove Realm ,add JsonModel

--- TimeFaceFoundation/Model/TFModel.m
+++ TimeFaceFoundation/Model/TFModel.m
@@ -7,9 +7,9 @@
 //
 
 #import "TFModel.h"
-#import <Realm/RLMObjectSchema.h>
-#import <Realm/RLMProperty.h>
-#import <Realm/RLMArray.h>
+//#import <Realm/RLMObjectSchema.h>
+//#import <Realm/RLMProperty.h>
+//#import <Realm/RLMArray.h>
 #import "AppMacro.h"
 
 @implementation TFModel
@@ -29,113 +29,113 @@
 //}
 
 
-- (id)initWithDictionary:(NSDictionary *)dict error:(NSError**)error{
-    self = [self init];
-    if (!dict) {
-        *error = [NSError errorWithDomain:APP_ERROR_DOMAIN
-                                    code:TFErrorCodeEmpty
-                                userInfo:nil];
-        return nil;
-    }
-    if (![dict isKindOfClass:[NSDictionary class]]) {
-        *error = [NSError errorWithDomain:APP_ERROR_DOMAIN
-                                     code:TFErrorCodeClassType
-                                 userInfo:nil];
-        return nil;
-    }
-    if (self) {
-        if (self.objectSchema) {
-            NSArray *properties = self.objectSchema.properties;
-            NSMutableDictionary *toInitDic = [[NSMutableDictionary alloc]initWithDictionary:dict];
-            for (RLMProperty *prop in properties) {
-                TFLog(@"pror = %@",prop);
-                id obj = [dict objectForKey:prop.name];
-                if (!obj) {
-                    if (prop.type == RLMPropertyTypeInt) {
-                        obj = [NSNumber numberWithInt:0];
-                    }else if (prop.type == RLMPropertyTypeDouble){
-                        obj = [NSNumber numberWithDouble:0];
-                    }else if (prop.type == RLMPropertyTypeBool){
-                        obj = [NSNumber numberWithBool:0];
-                    }else if (prop.type == RLMPropertyTypeFloat){
-                        obj = [NSNumber numberWithFloat:0];
-                    }else if(prop.type == RLMPropertyTypeString){
-                        obj = [NSString string];
-                    }else if (prop.type == RLMPropertyTypeObject){
-                        Class someClass = NSClassFromString(prop.objectClassName);
-                        NSError *error = nil;
-                        obj = [[someClass alloc] initWithDictionary:nil error:&error];
-                    }else if (prop.type == RLMPropertyTypeArray){
-                        obj = [NSArray array];
-                    }else if (prop.type == RLMPropertyTypeDate){
-                        obj = [NSDate date];
-                    }else if (prop.type == RLMPropertyTypeData){
-                        obj = [NSData data];
-                    }else if(prop.type == RLMPropertyTypeAny){
-                        obj = [NSObject new];
-                    }
-                    [toInitDic setObject:obj forKey:prop.name];
-                }else {
-                    if (prop.type == RLMPropertyTypeArray) {
-                        Class someClass = NSClassFromString(prop.objectClassName);
-                        NSError *error = nil;
-                        NSArray *dataDic = [dict objectForKey:prop.name];
-                        NSMutableArray *array = [[NSMutableArray alloc]initWithCapacity:10];
-                        for (NSDictionary *subDic in dataDic) {
-                            id object = [[someClass alloc] initWithDictionary:subDic error:&error];
-                            [array addObject:object];
-                        }
-                        [toInitDic setObject:array forKey:prop.name];
-                        
-                    }
-                }
-            }
-            self = [self initWithValue:toInitDic];
-        }
-    }else {
-        *error = [NSError errorWithDomain:APP_ERROR_DOMAIN
-                                     code:TFErrorCodeEmpty
-                                 userInfo:nil];
-        return nil;
-    }
-    return self;
-}
-
-- (NSDictionary*)toDictionary {
-    
-    NSMutableDictionary *dictionary = [[NSMutableDictionary alloc]init];
-    
-    if (self.objectSchema) {
-        NSArray *properties = self.objectSchema.properties;
-        for (RLMProperty *prop in properties) {
-            if (prop.type == RLMPropertyTypeObject) {
-                TFModel *object = [self valueForKey:prop.name];
-                NSDictionary *dic = [object toDictionary];
-                [dictionary setObject:dic forKey:prop.name];
-            }else if (prop.type == RLMPropertyTypeArray) {
-                RLMArray *rlmArray = [self valueForKey:prop.name];
-                NSMutableArray *array = [[NSMutableArray alloc]init];
-                for (TFModel *object in rlmArray) {
-                    NSDictionary *dic = [object toDictionary];
-                    [array addObject:dic];
-                }
-                [dictionary setObject:array forKey:prop.name];
-            }else {
-                NSString *value = [self valueForKey:prop.name];
-                TFLog(@"prop = %@ value = %@",prop,value);
-                if (!value) {
-                    value = [NSString string];
-                }
-                [dictionary setObject:value forKey:prop.name];
-            }
-            
-            
-        }
-    }
-    
-    
-    
-    return dictionary;
-}
+//- (id)initWithDictionary:(NSDictionary *)dict error:(NSError**)error{
+//    self = [self init];
+//    if (!dict) {
+//        *error = [NSError errorWithDomain:APP_ERROR_DOMAIN
+//                                    code:TFErrorCodeEmpty
+//                                userInfo:nil];
+//        return nil;
+//    }
+//    if (![dict isKindOfClass:[NSDictionary class]]) {
+//        *error = [NSError errorWithDomain:APP_ERROR_DOMAIN
+//                                     code:TFErrorCodeClassType
+//                                 userInfo:nil];
+//        return nil;
+//    }
+//    if (self) {
+//        if (self.objectSchema) {
+//            NSArray *properties = self.objectSchema.properties;
+//            NSMutableDictionary *toInitDic = [[NSMutableDictionary alloc]initWithDictionary:dict];
+//            for (RLMProperty *prop in properties) {
+//                TFLog(@"pror = %@",prop);
+//                id obj = [dict objectForKey:prop.name];
+//                if (!obj) {
+//                    if (prop.type == RLMPropertyTypeInt) {
+//                        obj = [NSNumber numberWithInt:0];
+//                    }else if (prop.type == RLMPropertyTypeDouble){
+//                        obj = [NSNumber numberWithDouble:0];
+//                    }else if (prop.type == RLMPropertyTypeBool){
+//                        obj = [NSNumber numberWithBool:0];
+//                    }else if (prop.type == RLMPropertyTypeFloat){
+//                        obj = [NSNumber numberWithFloat:0];
+//                    }else if(prop.type == RLMPropertyTypeString){
+//                        obj = [NSString string];
+//                    }else if (prop.type == RLMPropertyTypeObject){
+//                        Class someClass = NSClassFromString(prop.objectClassName);
+//                        NSError *error = nil;
+//                        obj = [[someClass alloc] initWithDictionary:nil error:&error];
+//                    }else if (prop.type == RLMPropertyTypeArray){
+//                        obj = [NSArray array];
+//                    }else if (prop.type == RLMPropertyTypeDate){
+//                        obj = [NSDate date];
+//                    }else if (prop.type == RLMPropertyTypeData){
+//                        obj = [NSData data];
+//                    }else if(prop.type == RLMPropertyTypeAny){
+//                        obj = [NSObject new];
+//                    }
+//                    [toInitDic setObject:obj forKey:prop.name];
+//                }else {
+//                    if (prop.type == RLMPropertyTypeArray) {
+//                        Class someClass = NSClassFromString(prop.objectClassName);
+//                        NSError *error = nil;
+//                        NSArray *dataDic = [dict objectForKey:prop.name];
+//                        NSMutableArray *array = [[NSMutableArray alloc]initWithCapacity:10];
+//                        for (NSDictionary *subDic in dataDic) {
+//                            id object = [[someClass alloc] initWithDictionary:subDic error:&error];
+//                            [array addObject:object];
+//                        }
+//                        [toInitDic setObject:array forKey:prop.name];
+//                        
+//                    }
+//                }
+//            }
+//            self = [self initWithValue:toInitDic];
+//        }
+//    }else {
+//        *error = [NSError errorWithDomain:APP_ERROR_DOMAIN
+//                                     code:TFErrorCodeEmpty
+//                                 userInfo:nil];
+//        return nil;
+//    }
+//    return self;
+//}
+//
+//- (NSDictionary*)toDictionary {
+//    
+//    NSMutableDictionary *dictionary = [[NSMutableDictionary alloc]init];
+//    
+//    if (self.objectSchema) {
+//        NSArray *properties = self.objectSchema.properties;
+//        for (RLMProperty *prop in properties) {
+//            if (prop.type == RLMPropertyTypeObject) {
+//                TFModel *object = [self valueForKey:prop.name];
+//                NSDictionary *dic = [object toDictionary];
+//                [dictionary setObject:dic forKey:prop.name];
+//            }else if (prop.type == RLMPropertyTypeArray) {
+//                RLMArray *rlmArray = [self valueForKey:prop.name];
+//                NSMutableArray *array = [[NSMutableArray alloc]init];
+//                for (TFModel *object in rlmArray) {
+//                    NSDictionary *dic = [object toDictionary];
+//                    [array addObject:dic];
+//                }
+//                [dictionary setObject:array forKey:prop.name];
+//            }else {
+//                NSString *value = [self valueForKey:prop.name];
+//                TFLog(@"prop = %@ value = %@",prop,value);
+//                if (!value) {
+//                    value = [NSString string];
+//                }
+//                [dictionary setObject:value forKey:prop.name];
+//            }
+//            
+//            
+//        }
+//    }
+//    
+//    
+//    
+//    return dictionary;
+//}
 
 @end
--- TimeFaceFoundation/Utility/TFDataHelper/TFDataHelper.h
+++ TimeFaceFoundation/Utility/TFDataHelper/TFDataHelper.h
@@ -7,58 +7,114 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Realm/RLMRealm.h>
-#import <Realm/RLMResults.h>
-#import <Realm/RLMObject.h>
+
+
+@class UserModel;
+@class ViewGuideModel;
+
 
 @interface TFDataHelper : NSObject
 
 
-@property (nonatomic, strong)RLMRealm  *defaultRealm;
 
 +(instancetype) shared;
 
-/**
- *  存储一个对象
- *
- *  @param object
- */
-- (void)saveObject:(RLMObject*)object;
 
 /**
- *  通过一个key-value获取一个指定类型对象(value为字符类型)
- *
- *  @param key
- *  @param value
- *  @param className
- *
- *  @return
- */
-- (RLMResults*)getObjectsWithKey:(NSString*)key strValue:(NSString*)value class:(Class)className;
-/**
- *  通过一个key-value获取一个指定类型对象(value为数字类型)
- *
- *  @param key
- *  @param value
- *  @param className
- *
- *  @return
- */
-- (RLMResults*)getObjectsWithKey:(NSString *)key numValue:(NSNumber *)value class:(Class)className;
-/**
- *  获取一个类型所有的对象
- *
- *  @param className
- *
- *  @return
- */
-- (RLMResults*)getAllObject:(Class)className;
-/**
- *  删除指定数据
+ *  保存／更新数据
  *
  *  @param object
  */
-- (void)removeObject:(RLMObject*)object;
+-(void) save:(id)object objId:(NSString *)objId;
+
+/**
+ *  获取一个对象数据
+ *
+ *  @param aClass
+ *  @param ident
+ *
+ *  @return
+ */
+-(id) getObject:(NSString *)className objId:(NSString *)objId;
+//-(id) getObjectByClass:(Class *)aClass objId:(NSString *)objId;
+
+/**
+ *  获取所有数据
+ *
+ *  @param aClass
+ *
+ *  @return
+ */
+-(NSArray *) getAll:(Class)aClass;
+
+/**
+ *  删除对应数据
+ *
+ *  @param object
+ */
+-(void) remove:(Class)aClass objId:(NSString *)objId;
+
+/**
+ *  删除所有数据
+ *
+ *  @param aClass
+ */
+-(void) removeAll:(Class)aClass;
+
+/**
+ *  保存引导信息
+ */
+-(void)saveViewGuideData;
+/**
+ *  获取引导信息
+ *
+ *  @param viewId
+ *
+ *  @return 
+ */
+-(ViewGuideModel *) loadViewGuideWithViewId:(NSString *)viewId;
+
+///**
+// *  存储一个对象
+// *
+// *  @param object
+// */
+//- (void)saveObject:(RLMObject*)object;
+//
+///**
+// *  通过一个key-value获取一个指定类型对象(value为字符类型)
+// *
+// *  @param key
+// *  @param value
+// *  @param className
+// *
+// *  @return
+// */
+//- (RLMResults*)getObjectsWithKey:(NSString*)key strValue:(NSString*)value class:(Class)className;
+///**
+// *  通过一个key-value获取一个指定类型对象(value为数字类型)
+// *
+// *  @param key
+// *  @param value
+// *  @param className
+// *
+// *  @return
+// */
+//- (RLMResults*)getObjectsWithKey:(NSString *)key numValue:(NSNumber *)value class:(Class)className;
+///**
+// *  获取一个类型所有的对象
+// *
+// *  @param className
+// *
+// *  @return
+// */
+//- (RLMResults*)getAllObject:(Class)className;
+///**
+// *  删除指定数据
+// *
+// *  @param object
+// */
+//- (void)removeObject:(RLMObject*)object;
 
 
 
